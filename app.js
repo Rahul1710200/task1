bd53ede9763d92392f8b77f9cf7acaa4b1abcf0c
Rahul Garg <rahulgarg17102001@gmail.com>
2024-08-05 16:39:31 +0530

    change

--- app.js
+++ app.js
@@ -1,91 +1,90 @@
-require('dotenv').config()
-const express=require('express');
-const serverless=require('serverless-http')
-const userModel = require('./models/user');
-const bcrypt = require('bcrypt');
-const path=require('path');
+require("dotenv").config();
+const express = require("express");
+const serverless = require("serverless-http");
+const userModel = require("./models/user");
+const bcrypt = require("bcrypt");
+const path = require("path");
 const app = express();
-const router=express.Router();
-const PORT=process.env.PORT || 3000
+const router = express.Router();
 
-
-
+// Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
-app.set('view engine', 'ejs');
-
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/.netlify/functions/app',router)
+// Define routes
+router.get("/", (req, res) => {
+  res.render("index");
+});
 
+router.get("/read", async (req, res) => {
+  try {
+    let users = await userModel.find();
+    res.render("read", { users });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
+router.get("/delete/:id", async (req, res) => {
+  try {
+    await userModel.findOneAndDelete({ _id: req.params.id });
+    res.redirect("/read");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
+router.post("/create", async (req, res) => {
+  try {
+    let {
+      firstName,
+      lastName,
+      number,
+      email,
+      street,
+      city,
+      state,
+      country,
+      login,
+      password,
+    } = req.body;
 
-app.get('/',(req,res)=>{
-    res.render("index")
-})
-app.get('/read',async (req,res)=>{
-    
-     try {
-       let users = await userModel.find();
-       res.render("read", { users });
-     } catch (error) {
-       console.error(error);
-       res.status(500).send("Internal Server Error");
-     }
-})
-app.get('/delete/:id',async (req,res)=>{
-    
-     try {
-       let users = await userModel.findOneAndDelete({_id:req.params.id});
-       res.redirect("/read")
-
-     } catch (error) {
-       console.error(error);
-       res.status(500).send("Internal Server Error");
-     }
-})
-app.post('/create',async (req,res)=>{
-   try {
-        let { firstName, lastName, number, email, street, city, state, country, login, password } = req.body;
-
-        
-        let existingEmailUser = await userModel.findOne({ email });
-        if (existingEmailUser) {
-          return res.status(400).send("User with this email already exists.");
-        }
-
-        let existingLoginUser = await userModel.findOne({ login });
-        if (existingLoginUser) {
-          return res.status(400).send("User with this login already exists.");
-        }
-        // Hashing the password
-        // const hashedPassword = await bcrypt.hash(password, 10);m
-
-        let createdUser = await userModel.create({
-            firstName,
-            lastName,
-            number,
-            email,
-            street,
-            city,
-            state,
-            country,
-            login,
-            password
-        });
-        console.log(createdUser);
-        res.redirect('/read')
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Internal Server Error");
+    let existingEmailUser = await userModel.findOne({ email });
+    if (existingEmailUser) {
+      return res.status(400).send("User with this email already exists.");
     }
 
-})
-app.listen(PORT,(err)=>{
-    if(err){
-        console.log(err);
+    let existingLoginUser = await userModel.findOne({ login });
+    if (existingLoginUser) {
+      return res.status(400).send("User with this login already exists.");
     }
-    console.log(`server started at port ${PORT}`);
-})
-module.exports.handler=serverless(app)
+
+    // Hashing the password
+    // const hashedPassword = await bcrypt.hash(password, 10);
+
+    await userModel.create({
+      firstName,
+      lastName,
+      number,
+      email,
+      street,
+      city,
+      state,
+      country,
+      login,
+      password,
+    });
+
+    res.redirect("/read");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Set up Netlify function handler
+module.exports.handler = serverless(app);
