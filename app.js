f7b834ace9533f03a42882d8cab24fa29056cefc
Rahul Garg <rahulgarg17102001@gmail.com>
2024-08-05 16:06:10 +0530

    change

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 require('dotenv').config()
 const express=require('express');
+const serverless=require('serverless-http')
 const userModel = require('./models/user');
 const bcrypt = require('bcrypt');
 const path=require('path');
 const app = express();
-
+const router=express.Router();
 const PORT=process.env.PORT || 3000
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use('/.netlify/functions/app',router)
 
 
 
@@ -83,3 +88,4 @@ app.listen(PORT,(err)=>{
     }
     console.log(`server started at port ${PORT}`);
 })
+module.exports.handler=serverless(app)
