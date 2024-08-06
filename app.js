37cb86349db92675d801cf52c835170e8a55d629
Rahul Garg <rahulgarg17102001@gmail.com>
2024-08-06 23:15:35 +0530

    change

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ const PORT=process.env.PORT || 3000
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
+app.set("views", path.join(__dirname, "frontend","views"));
 app.set('view engine', 'ejs');
+
 
 app.use(express.static(path.join(__dirname, "public")));
 
